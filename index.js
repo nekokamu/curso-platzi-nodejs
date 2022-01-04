64aca55671035cf77a2d2d7e7353672e545f34eb
nekokamu <spaletcano@gmail.com>
2022-01-04 18:34:00 -0300

    Se instaló express.js y se creó el servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo en servidor');
+});
+
+app.listen(port, () => {
+  console.log('Hola, el puerto es ' + port);
+});
