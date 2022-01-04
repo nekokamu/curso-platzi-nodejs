f5ec393046de8596feb2e375c62e9cd73a2786eb
nekokamu <spaletcano@gmail.com>
2022-01-04 18:39:18 -0300

    Se agregó routing con express.js

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/', (req, res) => {
   res.send('Hola mundo en servidor');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, esta es una nueva ruta o endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
 });
 
 app.listen(port, () => {
